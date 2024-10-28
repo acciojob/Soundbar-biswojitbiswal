cdc164fcdbad2ffdb2c175ab10a182cd924a47fd
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-28 10:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const audio = document.getElementById('audio');
+
+const buttons = document.querySelectorAll('.btn');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        const soundFile = button.getAttribute('data-sound');
+
+        audio.src = soundFile;
+        audio.play();
+    });
+});
+
+
+document.querySelector('.stop').addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+});
